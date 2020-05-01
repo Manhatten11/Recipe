ce4b37449d81b94ac435c32e0a886d0a52c1cfd6
Manhatten11 <nyamjargal_p@yahoo.com>
2020-05-02 00:58:09 +1000

    docs folder uusgelee git page ashiglahiin tuld

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,23 @@
-const arr = [12, 33, 122];
+// const arr = [12, 33, 122];
 
-let myFunc = (a) => {
-  console.log(`too : ${a}`);
-};
+// let myFunc = (a) => {
+//   console.log(`too : ${a}`);
+// };
 
-const arr2 = [...arr, 44, 565];
+// const arr2 = [...arr, 44, 565];
 
-myFunc(arr2[1]);
+// myFunc(arr2[1]);
+
+// import p from "lodash";
+// import query from "./model/Search";
+// import { add, multiply as mul } from "./view/searchView";
+
+// console.log("Хайлт : " + query);
+
+// console.log("Хоёр тооны нийлбэр :" + mul(4, 3));
+require("@babel/polyfill");
+import Search from "./model/Search";
+
+let search = new Search("pasta");
+
+search.doSearch().then((r) => console.log(r));
