0ddb3869a2cd762f8c49dffebaaaf496a569324f
Manhatten11 <nyamjargal_p@yahoo.com>
2020-05-02 03:02:23 +1000

    searchView hiilee

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -1,5 +1,34 @@
-export const add = (a, b) => a + b;
+import { elements } from "./base";
 
-export let multiply = (a, b) => a * b;
+const renderRecipe = (recipe) => {
+  console.log(recipe);
+  const markup = `
+         <li>
+            <a class="results__link results__link--active" href="#${recipe.recipe_id}">
+              <figure class="results__fig">
+                <img src="${recipe.image_url}" alt="Test" />
+              </figure>
+              <div class="results__data">
+                <h4 class="results__name">${recipe.title}</h4>
+                <p class="results__author">${recipe.publisher}</p>
+              </div>
+            </a>
+          </li>
+  `;
 
-export const id = 25;
+  //   ul рүүгээ нэмнэ.
+  elements.searchResultList.insertAdjacentHTML("beforeend", markup);
+};
+
+export const clearSearchQuery = () => {
+  elements.searchInput.value = "";
+};
+
+export const clearSearchResult = () => {
+  elements.searchResultList.innerHTML = "";
+};
+
+export const getInput = () => elements.searchInput.value;
+export const renderRecipes = (recipes) => {
+  recipes.forEach(renderRecipe);
+};
