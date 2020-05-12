826fe9bf895cf3ffd331e6eeab569175a8e04e8a
Manhatten11 <nyamjargal_p@yahoo.com>
2020-05-12 17:24:59 +1000

    replace

--- src/js/view/base.js
+++ src/js/view/base.js
@@ -1,5 +1,16 @@
 export const elements = {
   searchForm: document.querySelector(".search"),
   searchInput: document.querySelector(".search__field"),
+  searchResultDiv: document.querySelector(".results"),
   searchResultList: document.querySelector(".results__list"),
+};
+
+export const renderLoader = (parent) => {
+  const loader = `
+    <div class="loader">
+      <svg>
+        <use href="img/.icons.svg#icon-cw"></use>
+      </svg>
+    </div>
+  `;
 };
