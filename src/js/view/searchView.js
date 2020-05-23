a50e3430e4db27bce9e566b81c57afb3bc1a316b
Manhatten11 <nyamjargal_p@yahoo.com>
2020-05-23 16:19:52 +1000

    replaced

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -29,6 +29,32 @@ export const clearSearchResult = () => {
 };
 
 export const getInput = () => elements.searchInput.value;
-export const renderRecipes = (recipes) => {
-  recipes.forEach(renderRecipe);
+
+export const renderRecipes = (recipes, currentPage = 1, resPerPage = 10) => {
+  // Хайлтын үр дүнг хуудаслаж үзүүлэх
+  // page = 2, start =10, end 20
+  const start = (currentPage - 1) * resPerPage;
+  const end = currentPage * resPerPage;
+
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  // Хуудаслалтын товчуудыг гаргаж ирэх
+  const totalPages = Math.ceil(recipes / resPerPage);
+  renderButtons(currentPage, totalPages);
 };
+
+const renderButtons => (currentPage, totalPages) {
+  let button;
+
+  if(currentPage === 1 && totalPages > 1) {
+    // 1-р хуудсан дээр байна, 2-р хуудас гэдэг товчийг гарга
+  } else if(currentPage < totalPages) {
+    // Өмнөх болон дараачийн хуудас руу шилжүүлэх товчуудыг үзүүл
+  }
+  else if (currentPage === totalPages) {
+    // Хамгийн сүүлийн хуудсан дээр байна. Өмнөх рүү шилжүүлэх товчийг л үзүүлнэ.
+  }
+}
+
+// type ===> `prev`, `next`
+const createButton = (page, type) =>
